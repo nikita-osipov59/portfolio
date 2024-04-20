85e869e4fc61c0894acc0c3e298db9f8d69e7170
nikita-osipov59 <67689788+nikita-osipov59@users.noreply.github.com>
2024-04-20 22:45:05 +0300

    fix

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,8 @@
 import React from "react";
-<<<<<<< Updated upstream
-
-function App() {
-  return <div>app</div>;
-=======
 import { Router } from "./router";
 
 function App() {
   return <Router />;
->>>>>>> Stashed changes
 }
 
 export default App;
